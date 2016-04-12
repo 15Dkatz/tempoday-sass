e529b7e3fd274f1d7f1625a0fe593aa85008618e
davidkatz <15Dkatz@shcp.edu>
2016-04-12 11:29:46 -0700

    added game logic

--- www/scripts/game.js
+++ www/scripts/game.js
@@ -5,242 +5,252 @@ myApp.controller('GameController', ['$scope', '$rootScope', 'Authentication', 's
 
     $scope.testText = "dg fg";
 
-    // use a combination of on-tap to start a time
-    // and on-release to keep track of a time finished
 
-    // have a predetermined rhythm as represented through numeric values.
-    // the numeric value both outlines how the user should tap his or her rhythm
-    // as well as how the program displays notes in the rhythmDisplay
+    //48 = whole note, 24 = half note, 12 = quarter note, 6 = eigth note, 3 = sixteenth note. 8-8-8 = quarter triplet.
+    var noteValues = [3, 6, 12, 18, 24, 36, 48];
 
-    // after you track the user's rhythm
-    // compare this rhythm to the gameRhythm and give a response of how accurately the user tapped the screen.
+    $scope.globalLevel=1;
+
+    $scope.changeLevel = function() {
+        if ($scope.globalLevel===3) {
+            $scope.globalLevel=0
+        }
+        $scope.globalLevel++;
+    }
+
+    var generateGameRhythm = function(level) {
+        var gameRhythm = [];
+        // var limit = 192;
+        if (level===1) {
+            limit=72;
+        } else if (level===2) {
+            limit = 96;
+        } else {
+            limit = 192;
+        }
+        var randIndex;
+        var total=0;
+        while (limit>0) {
+            var randIndex = Math.floor(Math.random()*6);
+            if (limit<48) {
+                randIndex = Math.floor(Math.random()*5);
+            }
+            if (limit<36) {
+                randIndex = Math.floor(Math.random()*4);
+            }
+            if (limit<24) {
+                randIndex = Math.floor(Math.random()*3);
+            }
+            if (limit<18) {
+                randIndex = Math.floor(Math.random()*2);
+            }
+            if (limit<12) {
+                randIndex = Math.floor(Math.random()*1);
+            }
+            if (limit<6) {
+                randIndex = Math.floor(Math.random());
+            }
+            var randNoteVal = noteValues[randIndex];
+            
+            if (randNoteVal!=24) {
+                gameRhythm.push(randNoteVal);
+            } else {
+                randChance = Math.random();
+                if (randChance>.33) {
+                    gameRhythm.push(randNoteVal)
+                } else {
+                    for (var i=0; i<3; i++) {
+                        gameRhythm.push(8);
+                    }
+                }
+            }
+            total+=randNoteVal;
+            limit -= randNoteVal;
+
+        }
+        console.log(total);
+        return gameRhythm;
+    }
+
+    var timeItv;
+    $scope.time = 0;
+
+    var userRhythm = [];
+
+    $scope.accuracy;
+
+    var incrementTime = function() {
+        $scope.time+=1;
+    }
+
+    //new wad sound
+    // var sineSound = new Wad({source: 'sine'})
 
 
-    // var exerciseList;
-    // var exerciseTimeLimit;
+    $scope.startTime = function() {
+        timeItv = setInterval(incrementTime, 10);
+    }
+
+    $scope.stopTime = function() {
+        console.log(userRhythm.length, "UR l")
+        userRhythm.push($scope.time);
+        clearInterval(timeItv);
+        $scope.time=0;
+        if (userRhythm.length == gameRhythm.length) {
+            console.log("compare", gameRhythm, "to", userRhythm)
+            console.log("run the compare function!");
+            var accuracy = (compareRhythms(gameRhythm, userRhythm)*100).toFixed(2);
+            console.log("accuracy", accuracy);
+            $scope.accuracy=accuracy;
+            userRhythm = [];
+        }
+
+    }
+
+    var gameRhythm;
+    $scope.gameRhythmDisplay="";
+
+
+    // var noteValues = [3, 6, 12, 18, 24, 36, 48];
+
+    var numPairs = {
+        3: "s",
+        6: "e",
+        12: "q",
+        18: "q.",
+        24: "h",
+        36: "h.",
+        48: "w",
+
+    }
+
+    var rhythmDisplay = function(rhythm) {
+        var rhythmDisplay = "$4 \u00A0";
+        for (var i=0; i<rhythm.length; i++) {
+            var letter;
+            if (rhythm[i]===8) {
+                rhythmDisplay+= "Pqqq";
+                i+=2;
+            }
+            else if ((i<rhythm.length-3)&&((rhythm[i]===3)&&(rhythm[i+1]===3)&&(rhythm[i+2]===3)&&(rhythm[i+3]===3))) {
+                rhythmDisplay+="dffg";
+                i+=3;
+            }
+            else if ((i<rhythm.length-2)&&((rhythm[i]===3)&&(rhythm[i+1]===3)&&(rhythm[i+2]===6))) {
+                rhythmDisplay+="dgy";
+                i+=2;
+            }
+            else if ((i<rhythm.length-2)&&((rhythm[i]===3)&&(rhythm[i+1]===6)&&(rhythm[i+2]===6))) {
+                rhythmDisplay+="dty";
+                i+=2;
+            }       
+            else if ((i<rhythm.length-2)&&((rhythm[i]===6)&&(rhythm[i+1]===3)&&(rhythm[i+2]===3))) {
+                rhythmDisplay+="rdg";
+                i+=2;
+            }
+            else if ((i<rhythm.length-1)&&((rhythm[i]===6)&&(rhythm[i+1]===6))) {
+                rhythmDisplay+="ry";
+                i+=1;
+            } 
+            else if ((i<rhythm.length-1)&&((rhythm[i]===6)&&(rhythm[i+1]===3))) {
+                rhythmDisplay+="rg";
+                i+=1;
+            }
+            else if ((i<rhythm.length-1)&&((rhythm[i]===3)&&(rhythm[i+1]===6))) {
+                rhythmDisplay+="dy";
+                i+=1;
+            }
+            else if ((i<rhythm.length-1)&&((rhythm[i]===3)&&(rhythm[i+1]===3))) {
+                rhythmDisplay+="dg";
+                i+=1;
+            }           
+            else {
+                letter = numPairs[rhythm[i]];
+                // 15% chance of rest instead of normal note
+                var randRest = Math.random();
+                if (randRest<=.15) {
+                    letter = letter.toUpperCase();
+                    // remove restNote and go back an index
+                    // gameRhythm.splice(i,1);
+                    rhythm.splice(i, 1);
+                    gameRhythm = rhythm;
+                    i-=1;
+                    // gameRhythm[i] = 1;
+                }
+                rhythmDisplay += letter;
+                letter = letter.toLowerCase();
+            }
+            
+            switch(letter) {
+                case "e":
+                    rhythmDisplay += "\u00A0";
+                    break;
+                case "q":
+                    rhythmDisplay += "\u00A0\u00A0";
+                    break;
+                case "q.":
+                    rhythmDisplay += "\u00A0\u00A0\u00A0";
+                    break;
+                case "h":
+                    rhythmDisplay += "\u00A0\u00A0\u00A0\u00A0";
+                    break;
+                case "h.":
+                    rhythmDisplay += "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0";
+                    break;
+                case "w":
+                    rhythmDisplay += "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0";
+                    break;
+                default:
+                    rhythmDisplay += "";
+                    break;  
+            }
+        }
+        console.log(rhythmDisplay, "rd");
+        return rhythmDisplay;
+    }
+
+    var randBackground = ["green", "orange", "deep-orange", "light-blue", "teal", "blue", "red", "pink"];
+    var previous=undefined;
     
-    // $scope.currentExercise;
-    // $scope.exerciseList;
+    $scope.generateGRhythm = function() {
+        //debugging
+        // console.log(currentPage, "CP");
+        gameRhythm = generateGameRhythm($scope.globalLevel);
+        $scope.gameRhythmDisplay=rhythmDisplay(gameRhythm); 
+        console.log(gameRhythm);
 
-    // $scope.showStartButton = true;
+        var randBIndex = Math.floor(Math.random()*randBackground.length);
+        // $("#page2").addClass(randBackground[randBIndex]);
+        previous = randBackground[randBIndex];
 
-    // $scope.initExList = function() {
-    //     exerciseList = sharedExercises.getExerciseList();
-    //     // $scope.showStartButton = sharedExercises.getStart
-    // }
+    }
 
-    // $scope.updateExerciseList = function() {
-    //     exerciseList = sharedExercises.getExerciseList();
-    //     if (exerciseList) {
-    //         if (exerciseList.length>0) {
-    //             $scope.currentExercise = exerciseList[0];
-    //             exerciseTimeLimit = $scope.currentExercise["time"];
-    //         }
-    //     }
-    //     $scope.exerciseList = exerciseList;
-    //     return exerciseList;
-    // }
+    $scope.tryAgain = function() {
+        userRhythm=[];
+        page3topage2();
+    }
 
-    // var updateExerciseVariables = function() {
-    //     $scope.exerciseList = sharedExercises.getExerciseList();
-    //     $scope.currentExercise = $scope.exerciseList[0];
+    var compareRhythms = function(rhythm1, rhythm2) {
+        // check if accurate
+        var rhythm1Divisor = rhythm1[0];
+        var rhythm1total = 0;
+        for (var r1=0; r1<rhythm1.length; r1++) {
+            rhythm1total+=(rhythm1[r1]/rhythm1Divisor);
+        }
 
-    //     // update firebase
-    //     // var exerciseListRef = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/exercises');
-    //     // // $scope.$apply(function() {
-    //     // exerciseListRef.update({"exerciseList": $scope.exerciseList});
-    //     // })
-    // }
+        var rhythm2Divisor = rhythm2[0];
+        var rhythm2total = 0;
+        for (var r2=0; r2<rhythm2.length; r2++) {
+            rhythm2total+=(rhythm2[r2]/rhythm2Divisor);
+        }
 
-    // auth.$onAuth(function(authUser) {
-    //     if (authUser) {
-    //         exerciseList = sharedExercises.getExerciseList();
-    //     }
-    // })
+        if (rhythm1total/rhythm2total>0&&rhythm1total/rhythm2total<1) {
+            return rhythm1total/rhythm2total;
+        } else {
+            return rhythm2total/rhythm1total;
+        }
+    }
 
-    // $scope.data = {
-    //     showReordering: false,
-    //     shouldShowDelete: false,
-    //     canSwipe: true,
-    // }
-
-    // $scope.reorderItem = function(item, fromIndex, toIndex) {
-    //     $scope.exerciseList.splice(fromIndex, 1)
-    //     $scope.exerciseList.splice(toIndex, 0, item)
-    // }
-
-    // // change color here later!
-    // var progressBarCircle = new ProgressBar.Circle("#progressBarCircle", {
-    //     color: '#ef473c',
-    //     strokeWidth: 8
-    // })
-
-
-    // $scope.editExercise = function(index) {
-    //     $scope.newExercise = {};
-    //     var myPopup = $ionicPopup.show({
-    //     template: "<input class='inputIndent' placeholder='Name' type='text' ng-model='newExercise.exercise'><br><input type='number' class='inputIndent' placeholder='Time' ng-model='newExercise.time'>",
-    //     title: 'Edit Exercise',
-    //     scope: $scope,
-    //     buttons: [
-    //       { text: 'Cancel' },
-    //       {
-    //         text: '<b>Save</b>',
-    //         type: 'button-positive',
-    //         onTap: function(e) {
-    //           if (!$scope.newExercise) {
-    //             e.preventDefault();
-    //           } else {
-    //             if ($scope.newExercise.exercise) {
-    //                 $scope.exerciseList[index]["exercise"] = $scope.newExercise.exercise;
-    //             }
-    //             if ($scope.newExercise.time) {
-    //                 $scope.exerciseList[index]["time"] = $scope.newExercise.time;  
-    //             }
-                
-    //             sharedExercises.setExerciseList($scope.exerciseList);
-    //             updateExerciseVariables();
-    //           }
-    //         }
-    //       }
-    //     ]
-    //   });
-
-    //   myPopup.then(function(res) {
-    //     console.log('Tapped!', res);
-    //   });
-
-    //   $timeout(function() {
-    //      myPopup.close(); 
-    //   }, 10000);
-    // }
-
-    // var timer;
-    // var exTime = 0;
-
-    // var addTime = function() {
-    //     // exTime=sharedExercises.getExTime();
-    //     exerciseTimeLimit = $scope.currentExercise["time"];
-    //     // console.log($scope.currentExercise["currentTime"], "currentTime");
-    //     exTime+=1;
-    //     // sharedExercises.setExTime(exTime);
-
-    //     console.log("exTime", exTime)
-    //     $scope.$apply(function() {
-    //         updateExerciseVariables();
-    //     });
-
-    //     if (exTime>exerciseTimeLimit) {
-    //         removeCurrentTask();
-    //         exTime=0;
-    //         // sharedExercises.setExTime(exTime);
-    //         // updateExerciseVariables();
-    //     }
-
-    //     progressBarCircle.animate(exTime/exerciseTimeLimit, function() {
-    //         progressBarCircle.setText(exTime);
-    //     });
-
-
-
-    // }
-
-    // $scope.$on('builtNewSet', function(event, args) {
-    //     console.log("receiving builtNewSet broadcast");
-    //     progressBarCircle.animate(0, function() {
-    //         progressBarCircle.setText(0);
-    //     });
-    //     exTime=0;
-    // });
-
-    // $scope.removeExercise = function(index) {
-    //     $scope.exerciseList.splice(index, 1);
-    //     sharedExercises.setExerciseList($scope.exerciseList);
-    //     // $scope.updateExerciseList();
-    //     // $scope.exerciseList = $scope.updateExerciseList();
-    //     timer = false;
-    //     updateExerciseVariables();
-    //     // exTime = $scope.currentExercise["time"];
-    // }
-
-    // $scope.skipExercise = function() {
-    //     exTime=0;
-    //     sharedExercises.setExTime(exTime);
-    //     removeCurrentTask();
-    // }
-
-
-    // var removeCurrentTask = function() {
-    //     $scope.exerciseList.shift();
-    //     console.log($scope.exerciseList);
-    //     sharedExercises.setExerciseList($scope.exerciseList);
-    //     updateExerciseVariables(); 
-    // }
-
-
-
-
-    // $scope.startExercise = function() {
-    //     // exerciseTimeLimit = $scope.currentExercise["time"];
-    //     $scope.showStartButton = false;
-    //     if (!timer) {
-    //         timer = setInterval(addTime, 1000);
-    //     }
-    // }
-
-    // $scope.pauseBtn = {
-    //     'name': 'pause',
-    //     'ionname': 'ion-pause'
-    // }
-
-    // $scope.pauseExercise = function() {
-    //     clearInterval(timer);
-    //     if ($scope.pauseBtn['name']=='pause') {
-    //         $scope.pauseBtn['name']='resume';
-    //         $scope.pauseBtn['ionname'] = 'ion-play';
-    //     } else {
-    //         timer = false;
-    //         $scope.pauseBtn['name']='pause';
-    //         $scope.pauseBtn['ionname'] = 'ion-pause';
-    //         $scope.startExercise();
-    //     }
-
-    // }
-
-    // $scope.addExercise = function() {
-    //     // popup template for new Exercise
-    //     $scope.newExercise = {};
-    //     var myPopup = $ionicPopup.show({
-    //     template: "<input class='inputIndent' placeholder='Name' type='text' ng-model='newExercise.exercise'><br><input type='number' class='inputIndent' placeholder='Time in seconds' ng-model='newExercise.time'>",
-    //     title: 'New Exercise',
-    //     scope: $scope,
-    //     buttons: [
-    //       { text: 'Cancel' },
-    //       {
-    //         text: '<b>Add</b>',
-    //         type: 'button-positive',
-    //         onTap: function(e) {
-    //           if (!$scope.newExercise) {
-    //             e.preventDefault();
-    //           } else {
-    //             $scope.exerciseList.push($scope.newExercise);
-    //             sharedExercises.setExerciseList($scope.exerciseList);
-    //             updateExerciseVariables();
-    //           }
-    //         }
-    //       }
-    //      ]
-    //     });
-
-    //     myPopup.then(function(res) {
-    //       console.log('Tapped!', res);
-    //     });
-
-    //     $timeout(function() {
-    //        myPopup.close(); 
-    //     }, 10000);
-
-    // }
 
 
 }]); // Controller
